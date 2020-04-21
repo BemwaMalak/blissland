92bd2c5d5d064982f46bd05a1259d81dbb639366
YoussefJoe <you26mos@gmail.com>
2020-04-21 03:10:29 +0200

    blisslandwebsite

---
+++ js/style.js
@@ -0,0 +1,11 @@
+.test
+$("button").click(function()
+$("div").animate({
+left:'250px'
+opacity:'0.5',
+height:'150px',
+width:'150px'
+ });
+});
+
+            $('.test').textillate({ in: { effect: 'rollIn' } });
